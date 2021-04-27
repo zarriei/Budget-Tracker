3f59d5d2349732b24d17a1de085c6010b62440dc
zarriei <75182441+zarriei@users.noreply.github.com>
2021-04-26 22:22:46 -0400

    api routes fixed

--- Develop/routes/server.js
+++ Develop/routes/server.js
@@ -21,7 +21,7 @@ mongoose.connect("mongodb://localhost/budget", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("../routes/api"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
